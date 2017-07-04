94a1f6d78a8c4456a4e6792367328517e1ad2fe2
MMaetha <mod41744555@hotmail.com>
2017-07-04 13:46:56 +0900

    change data in graph

--- src/Home/graph.js
+++ src/Home/graph.js
@@ -9,25 +9,21 @@ export default class Graph extends Component {
     var myChart = new Chart(ctx, {
       type: 'bar',
       data: {
-        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+        labels: ["Day 1", "Day 2", "Day 3", "Day 4", "Day 5", "Day 6", "Day 7"],
         datasets: [{
-          label: '# of Votes',
-          data: [12, 19, 3, 5, 2, 3],
+          label: 'Numbers of steps',
+          data: [5493, 3121, 4861, 7100, 21281, 3198, 2725],
+
           backgroundColor: [
-            'rgba(255, 99, 132, 0.2)',
-            'rgba(54, 162, 235, 0.2)',
-            'rgba(255, 206, 86, 0.2)',
-            'rgba(75, 192, 192, 0.2)',
-            'rgba(153, 102, 255, 0.2)',
-            'rgba(255, 159, 64, 0.2)'
+            'rgba(255, 99, 132, 1)',
+            'rgba(255, 206, 86, 1)',
+            'rgba(247, 205, 204, 1)',
+            'rgba(75, 192, 192, 1)',
+            'rgba(255, 159, 64, 1)',
+            'rgba(54, 162, 235, 1)',
+            'rgba(145, 168, 208, 1)'
           ],
           borderColor: [
-            'rgba(255,99,132,1)',
-            'rgba(54, 162, 235, 1)',
-            'rgba(255, 206, 86, 1)',
-            'rgba(75, 192, 192, 1)',
-            'rgba(153, 102, 255, 1)',
-            'rgba(255, 159, 64, 1)'
           ],
           borderWidth: 1
         }]
@@ -38,7 +34,12 @@ export default class Graph extends Component {
             ticks: {
               beginAtZero: true
             }
-          }]
+          }],
+          xAxes: [{
+                time: {
+                    unit: 'day'
+                }
+            }]
         }
       }
     });
